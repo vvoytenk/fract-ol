8cb0bc7e188d3d0ab2779c37aa026188ea94eb73
Victoria VOYTENKO <vvoytenk@e1r7p15.unit.ua>
2018-04-09 17:13:29 +0300

    beg

---
+++ cmake-build-debug/CMakeFiles/fractol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fractol.dir/main.c.o"
+  "fractol.pdb"
+  "fractol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fractol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
